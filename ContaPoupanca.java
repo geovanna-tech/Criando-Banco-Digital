eae37a153b23dba34fdc9f8cef1ebbe584a8858a
Geovanna Capinan <121056909+geovanna-tech@users.noreply.github.com>
2024-06-08 14:19:21 -0300

    Delete ContaPoupanca.java

--- ContaPoupanca.java
+++
@@ -1,8 +0,0 @@
-public class ContaPoupanca extends Conta {
-    public ContaPoupanca(String numero) {
-        super(numero);
-    }
-
-    // A ContaPoupanca pode ter métodos adicionais específicos
-}
-
